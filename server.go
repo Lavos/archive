4625935212bf84d6bc0a7c6bc05508adcbc3a288
Kristopher Cost <me@krisco.st>
2013-11-25 18:14:51 -0800

    master: getting hex to sha1sum conversion working

--- server.go
+++ server.go
@@ -1,11 +1,18 @@
 package archive
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
 	"os"
 	"github.com/hoisie/web"
+	"encoding/json"
+	"strconv"
+	"errors"
 )
+
+type PostJSON struct {
+	Title string `json:"title"`
+}
 
 type Server struct {
 	server *web.Server
@@ -18,13 +25,67 @@ func NewServer() *Server {
 		server: w,
 	}
 
-	w.Get("/note/(.*)", s.noteHandler)
+	w.Get("/note/([0-9a-f]{40})/?", s.getNoteRevisions)
+	w.Post("/note/", s.postNote)
+	w.Post("/note/([0-9a-f]{40})", s.postRevision)
 
 	return s
 }
 
-func (s *Server) (val string) string {
-	return fmt.Sprintf("hello! %#v - %#v", val, s.store.getRevision(val))
+func getSumFromString (hex string) ([]byte, error) {
+	sha1sum := make([]byte, 0)
+	var pair string
+	var val uint64
+	var err error
+
+	for len(hex) > 0 {
+		pair = hex[:2]
+		val, err = strconv.ParseUint(pair, 16, 8)
+
+		if err != nil {
+			return nil, errors.New("Could not parse int.")
+		}
+
+		sha1sum = append(sha1sum, uint8(val))
+		hex = hex[2:]
+	}
+
+	return sha1sum, nil
+}
+
+func (s *Server) getNoteRevisions (ctx *web.Context, hex string) string {
+	log.Printf("string: %v", hex);
+
+	sha1sum, err := getSumFromString(hex)
+
+	if err != nil {
+		ctx.Abort(500, "Could not get sha1sum from provided string.")
+		return ""
+	}
+
+	notebytes, err := s.store.getNote(sha1sum)
+
+	if err != nil {
+		ctx.Abort(500, "Could not get note with that sha1.")
+		return ""
+	}
+
+	return string(notebytes)
+}
+
+func (s *Server) postNote (ctx *web.Context) string {
+	var postjson PostJSON
+	json.Unmarshal([]byte(ctx.Params["json"]), &postjson)
+
+	log.Printf("params: %#v", ctx.Params)
+	log.Printf("postjson obj: %#v", postjson)
+
+	s.store.addNote(postjson.Title)
+	return "done"
+}
+
+func (s *Server) postRevision (val string) string {
+	
 }
 
 func (s *Server) Run () {
--- store.go
+++ store.go
@@ -2,6 +2,10 @@ package archive
 
 import (
 	"log"
+	// "fmt"
+	"errors"
+	"math/rand"
+	"time"
 	"github.com/cznic/kv"
 	"crypto/sha1"
 	"encoding/json"
@@ -12,21 +16,15 @@ var (
 	sub = make([]byte, 0)
 )
 
-
 type Store struct {
 	db *kv.DB
 }
 
 type Note struct {
+	Title string
+	signature int64
 	RevisionRefs [][]byte
 }
-
-type Revision struct {
-	Ref BlobRef
-	Blob []byte
-}
-
-type BlobRef []byte
 
 func newStore () *Store {
 	db, err := kv.Open("db.kv", &kv.Options{})
@@ -47,55 +45,83 @@ func newStore () *Store {
 		db: db,
 	}
 
+	enum, err := db.SeekFirst()
+
+	if err == nil {
+		key := make([]byte, 0)
+		value := make([]byte, 0)
+		err = nil
+
+		for ; err == nil; key, value, err = enum.Next() {
+			log.Printf("%#v: %v", key, string(value))
+		}
+	}
+
 	return s
 }
 
-func (s *Store) getRevision (title string) *Revision {
-	sub, _ = s.db.Get(buf, []byte(title))
+func (s *Store) getNote (b []byte) ([]byte, error) {
+	sub, _ = s.db.Get(buf, b)
 	log.Printf("%#v", string(sub))
 
-	var nn Note
-	json.Unmarshal(sub, &nn)
-
-	log.Printf("note nn: %#v", nn)
-
-	sub, _ = s.db.Get(buf, nn.RevisionRefs[0])
-	log.Printf("%#v", string(sub))
-
-	r := &Revision{
-		Ref: nn.RevisionRefs[0],
-		Blob: sub,
+	if sub == nil {
+		return nil, errors.New("Could not find not with specified sha1sum")
 	}
 
-	return r
+	return sub, nil
 }
 
-func (s *Store) addRevision () {
-	data := []byte("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.")
-	h := sha1.New()
-	h.Write(data)
-	sha1sum := h.Sum(nil)
+func (s *Store) getRevision (b []byte) ([]byte, error) {
+	log.Printf("%#v", b)
 
-	r := Revision{
-		Ref: sha1sum,
-		Blob: data,
+	sub, _ = s.db.Get(buf, b)
+	log.Printf("%#v", string(sub))
+
+	if sub == nil {
+		return nil, errors.New("Could not get revision with specified blobref: " + string(b))
 	}
 
-	n := Note{
+	return sub, nil
+}
+
+func (s *Store) addNote (title string) {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	n := &Note{
+		Title: title,
+		signature: rand.Int63(),
 		RevisionRefs: make([][]byte, 0),
 	}
 
-	n.RevisionRefs = append(n.RevisionRefs, r.Ref)
-
 	b, _  := json.Marshal(n)
 
-	log.Printf("%#v", b)
+	log.Printf("new note json: %#v", string(b))
 
-	s.db.BeginTransaction()
+	h := sha1.New()
+	h.Write(b)
+	sha1sum := h.Sum(nil)
 
-	s.db.Set([]byte("aaaaa"), b)
-	s.db.Set(r.Ref, r.Blob)
+	log.Printf("new note sha1: %x", sha1sum)
+	log.Printf("new note sha1: %#v", sha1sum)
 
-	s.db.Commit()
+	s.db.Set(sha1sum, b)
+	log.Printf(string(b))
+}
 
+func (s *Store) addRevision (targetRef []byte, content []byte) {
+	notebytes := s.getNote(target)
+	var n note
+	json.Unmarshal(notebytes, &n)
+
+	h := sha1.New()
+	h.write(content)
+	sha1sum := h.Sum(nil)
+
+	s.db.Set(sha1sum, content)
+
+	n.RevisionRefs = append(n.RevisionRefs, sha1sum)
+
+	b, _ := json.Marshal(n)
+
+	s.db.Set(targetRef, b)
 }
